385b9489c000dc93defe2b7a81b8e44af55bca0e
sergias <sergias@ukr.net>
2020-08-25 15:49:42 +0300

    lesson 13 - JavaConfig

--- src/main/java/ru/alishev/springcourse/ClassicalMusic.java
+++ src/main/java/ru/alishev/springcourse/ClassicalMusic.java
@@ -1,22 +1,18 @@
 package ru.alishev.springcourse;
 
-import org.springframework.stereotype.Component;
-
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
-@Component
-//@Scope("prototype")
 public class ClassicalMusic implements Music {
 
     @PostConstruct
     public void doInit() {
-        System.out.println("my init");
+        System.out.println("ClassicalMusic init");
     }
 
     @PreDestroy
     public void doDestroy() {
-        System.out.println("my destroy");
+        System.out.println("ClassicalMusic destroy");
     }
 
     @Override
